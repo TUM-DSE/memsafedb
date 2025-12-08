ca8cb15a697d0a6f5d59902a8c0b546ba3b5ee21
Martin Fink <martin@finkmartin.com>
2025-12-08 09:17:56 +0100

    wip jemalloc mte

--- utils/mte.hpp
+++ utils/mte.hpp
@@ -8,12 +8,20 @@
 #include <stdio.h>
 #include <arm_acle.h>
 
+#define MTE_GRANULE_SIZE 16
 #define MTE_MODE_SYNC 1
 #define MTE_MODE_ASYNC 2
 const uintptr_t addr_mask = 0x00FFFFFFFFFFFFFFULL;
 
+#ifdef MTE
+inline size_t round_to_tag_granule(size_t size) {
+  return (size + (MTE_GRANULE_SIZE - 1)) & ~(MTE_GRANULE_SIZE - 1);
+}
+#endif
+
+#define MTE_TAG_INCLUDE_MASK 0xfffe
 inline void init_process(int mode){
-  if (prctl(PR_SET_TAGGED_ADDR_CTRL, (1UL << 0) | PR_MTE_TCF_SYNC | (0xfffe << 3), 0, 0, 0)){
+  if (prctl(PR_SET_TAGGED_ADDR_CTRL, (1UL << 0) | PR_MTE_TCF_SYNC | (MTE_TAG_INCLUDE_MASK << 3), 0, 0, 0)){
     perror("prctl() failed");
   }
 }
@@ -24,13 +32,25 @@ inline void* tag_pointer(void* ptr, size_t size){
     perror("non aligned tagging");
   }
   assert(size % 16 == 0);
-  void *t_ptr;
-  __asm__ volatile("irg %0, %1" : "=r" (t_ptr): "r" (ptr));
-  for (size_t i = 0; i < size; i += 16) {
-    void *c_ptr = (void *)((uintptr_t)ptr + i);
-    __asm__ volatile("stg %0, [%1]" : : "r" (t_ptr), "r" (c_ptr) : "memory");
+  __asm__ volatile("irg %0, %1" : "+r" (ptr) : );
+  void *end = (char*) ptr + size;
+  void *ret;
+  while (ptr < end) {
+    __asm__ volatile("stg %0, [%1], #16" : "+r"(ptr) : : "memory");
   }
-  return t_ptr;
+  return ret;
+}
+
+inline void* tag_and_zero_memory_region(void* ptr, size_t size){
+  assert(ptr != NULL);
+  assert(size % 16 == 0);
+  __asm__ volatile("irg %0, %1" : "+r" (ptr) : );
+  void *end = (char*) ptr + size;
+  void *ret;
+  while (ptr < end) {
+    __asm__ volatile("stzg %0, [%1], #16" : "+r"(ptr) : : "memory");
+  }
+  return ret;
 }
 
 inline void* untag_pointer(void* ptr, size_t size){
@@ -38,11 +58,10 @@ inline void* untag_pointer(void* ptr, size_t size){
   assert(size % 16 == 0);
 
   void *ut_ptr = (void*)((uintptr_t)ptr & addr_mask);
-    
-  for (size_t i = 0; i < size; i += 16) {
-    void *c_ptr = (void *)((uintptr_t)ptr + i);
-    __asm__ volatile("stg %0, [%1]" : : "r" (ut_ptr), "r" (c_ptr) : "memory");
-  } 
+  void *end = (char*) ptr + size;
+  while (ptr < end) {
+    __asm__ volatile("stg %0, [%1], #16" : "+r" (ptr) : "r" (ut_ptr) : "memory");
+  }
   return ut_ptr;
 }
 
