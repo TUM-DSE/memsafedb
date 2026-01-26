3f23a2d3d27926d86ee8cadaefd3ad669c0cf74d
Martin Fink <martin@finkmartin.com>
2026-01-26 09:15:41 +0000

    make inline functions static

--- utils/mte.h
+++ utils/mte.h
@@ -17,18 +17,18 @@
 #define CONSTEXPR_MODIFIER
 #endif
 
-CONSTEXPR_MODIFIER inline size_t round_to_tag_granule(size_t size) {
+CONSTEXPR_MODIFIER static inline size_t round_to_tag_granule(size_t size) {
   return (size + (MTE_GRANULE_SIZE - 1)) & ~(MTE_GRANULE_SIZE - 1);
 }
 
 #define MTE_TAG_INCLUDE_MASK 0xfffe
-inline void init_process(int mode){
+static inline void init_process(int mode){
   if (prctl(PR_SET_TAGGED_ADDR_CTRL, (1UL << 0) | PR_MTE_TCF_SYNC | (MTE_TAG_INCLUDE_MASK << 3), 0, 0, 0)){
     perror("prctl() failed");
   }
 }
 
-inline void* tag_memory_region(void* ptr, size_t size) {
+static inline void* tag_memory_region(void* ptr, size_t size) {
   __asm__ volatile("irg %0, %0" : "+r" (ptr) : );
   void *end = (char*) ptr + size;
   void *ret = ptr;
@@ -38,7 +38,7 @@ inline void* tag_memory_region(void* ptr, size_t size) {
   return ret;
 }
 
-inline void* tag_and_zero_memory_region(void* ptr, size_t size){
+static inline void* tag_and_zero_memory_region(void* ptr, size_t size){
   __asm__ volatile("irg %0, %1" : "+r" (ptr) : );
   void *end = (char*) ptr + size;
   void *ret = ptr;
@@ -48,7 +48,7 @@ inline void* tag_and_zero_memory_region(void* ptr, size_t size){
   return ret;
 }
 
-inline void* untag_memory_region(void* ptr, size_t size){
+static inline void* untag_memory_region(void* ptr, size_t size){
   void *ut_ptr = (void*)((uintptr_t)ptr & ADDR_MASK);
   void *end = (char*) ptr + size;
   while (ptr < end) {
@@ -57,16 +57,16 @@ inline void* untag_memory_region(void* ptr, size_t size){
   return ut_ptr;
 }
 
-inline void* clear_tag(void *ptr){
+static inline void* clear_tag(void *ptr){
   return (void*)((uintptr_t)ptr & ADDR_MASK);
 }
 
-inline void* load_tag(void *ptr){
+static inline void* load_tag(void *ptr){
   __asm__ volatile("ldg %0, [%0]" : "+r"(ptr));
   return ptr;
 }
 
-inline void print_tag(void* ptr){
+static inline void print_tag(void* ptr){
   int logical_tag = ((uintptr_t) ptr) >> 56;
   void* alloc_tag= __arm_mte_get_tag(ptr);
   printf("ptr: %p, logical tag: %u, allocation tag: %lu\n", ptr, logical_tag, ((uintptr_t)alloc_tag)>>56);
